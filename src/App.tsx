a803228b2c11526d7dff7072be0991f669e49941
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2022-05-09 17:20:22 +0900

    feat: install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button, ChakraProvider } from '@chakra-ui/react';
 import { FC } from 'react';
+import { theme } from 'theme/theme';
 
 export const App: FC = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>ああああああ</p>
+    </ChakraProvider>
   );
 };
