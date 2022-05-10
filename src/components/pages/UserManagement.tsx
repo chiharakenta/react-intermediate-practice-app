b54034ad8e876d10299ce4925278741d39d99d91
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2022-05-11 08:57:31 +0900

    feat: getUsers

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,29 @@
-import { Wrap, WrapItem } from '@chakra-ui/react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
 import { UserCard } from 'components/organisms/user/UserCard';
-import { FC, memo } from 'react';
+import { useAllUsers } from 'hooks/useAllUsers';
+import { FC, memo, useEffect } from 'react';
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard imageUrl="https://source.unsplash.com/random/" userName="chihaken" fullName="Kenta Chihara" />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard imageUrl="https://source.unsplash.com/random/" userName={user.username} fullName={user.name} />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
